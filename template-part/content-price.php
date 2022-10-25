3392fcd5094d738a5540596b6940b850a7299017
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-25 10:31:20 +0600

    PHP code for loop post

--- template-part/content-price.php
+++ template-part/content-price.php
@@ -5,25 +5,97 @@
                 <h1 class="display-4">Awesome Medical Programs</h1>
             </div>
             <div class="owl-carousel price-carousel position-relative" style="padding: 0 45px 45px 45px;">
-                <div class="bg-light rounded text-center">
-                    <div class="position-relative">
-                        <img class="img-fluid rounded-top" src="<?php echo get_theme_file_uri('img/price-1.jpg');?>" alt="">
-                        <div class="position-absolute w-100 h-100 top-50 start-50 translate-middle rounded-top d-flex flex-column align-items-center justify-content-center" style="background: rgba(29, 42, 77, .8);">
-                            <h3 class="text-white">Pregnancy Care</h3>
-                            <h1 class="display-4 text-white mb-0">
-                                <small class="align-top fw-normal" style="font-size: 22px; line-height: 45px;">$</small>49<small class="align-bottom fw-normal" style="font-size: 16px; line-height: 40px;">/ Year</small>
-                            </h1>
+
+            <?php
+                    // the query (for Post Loop)
+
+                    $args = array(
+                        'post_type' => 'customtestimonial'
+                    );
+
+                    $the_query = new WP_Query( $args ); ?>
+
+                    <?php if ( $the_query->have_posts() ) : ?>
+
+                        <!-- pagination here -->
+
+                        <!-- the loop -->
+                        <?php while ( $the_query->have_posts() ) : $the_query->the_post();
+                        $image_url;
+                        // Image Location.
+                        if ( has_post_thumbnail() ) {
+                            $image_url = get_the_post_thumbnail_url(get_the_ID());
+                        }
+                        else {
+                            $image_url = get_bloginfo('stylesheet_directory') .'/img/about.jpg"';
+                        }
+
+                        ?>
+
+                        <div class="testimonial-item text-center">
+                            <div class="position-relative mb-5">
+                                <img class="img-fluid rounded-circle mx-auto" src="<?php echo $image_url; ?>" alt="">
+                                <div class="position-absolute top-100 start-50 translate-middle d-flex align-items-center justify-content-center bg-white rounded-circle" style="width: 60px; height: 60px;">
+                                    <i class="fa fa-quote-left fa-2x text-primary"></i>
+                                </div>
+                            </div>
+                            <p class="fs-4 fw-normal"><?php the_content(); ?></p>
+                            <hr class="w-25 mx-auto">
+                            <h3><?php the_title(); ?></h3>
+                                <?php
+                                    $postid = $the_query->post->ID;
+
+                                    $icon_class = get_post_meta($postid, 'profession', true);
+                                    ?>
+                                        <h5 class="fw-normal text-primary mb-3"><?php echo $icon_class; ?></h5>
+                                    <?php
+
+                                    // CMB2 Field Repeatable Fields
+                                    $entries = get_post_meta( $postid, 'tcompany', true );
+                                    if($entries){
+                                        foreach ( (array) $entries as $key ) {
+                                            ?>
+                                                <h6 class="fw-normal text-primary mb-3"><?php echo $key; ?></h6>
+                                            <?php
+                                        }
+                                    }
+                                ?>
                         </div>
-                    </div>
-                    <div class="text-center py-5">
-                        <p>Emergency Medical Treatment</p>
-                        <p>Highly Experienced Doctors</p>
-                        <p>Highest Success Rate</p>
-                        <p>Telephone Service</p>
-                        <a href="" class="btn btn-primary rounded-pill py-3 px-5 my-2">Apply Now</a>
-                    </div>
-                </div>
-                <div class="bg-light rounded text-center">
+
+                        <div class="bg-light rounded text-center">
+                            <div class="position-relative">
+                                <img class="img-fluid rounded-top" src="<?php echo $image_url; ?>" alt="">
+                                <div class="position-absolute w-100 h-100 top-50 start-50 translate-middle rounded-top d-flex flex-column align-items-center justify-content-center" style="background: rgba(29, 42, 77, .8);">
+                                    <h3 class="text-white">Pregnancy Care</h3>
+                                    <h1 class="display-4 text-white mb-0">
+                                        <small class="align-top fw-normal" style="font-size: 22px; line-height: 45px;">৳</small>49<small class="align-bottom fw-normal" style="font-size: 16px; line-height: 40px;">/ Year</small>
+                                    </h1>
+                                </div>
+                            </div>
+                            <div class="text-center py-5">
+                                <p>Emergency Medical Treatment</p>
+                                <p>Highly Experienced Doctors</p>
+                                <p>Highest Success Rate</p>
+                                <p>Telephone Service</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 my-2">Apply Now</a>
+                            </div>
+                        </div>
+
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->
+
+                        <!-- pagination here -->
+
+                        <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                    <?php endif;
+                    // End the query (for Post Loop)
+
+                ?>
+
+                <!-- <div class="bg-light rounded text-center">
                     <div class="position-relative">
                         <img class="img-fluid rounded-top" src="<?php echo get_theme_file_uri('img/price-2.jpg');?>" alt="">
                         <div class="position-absolute w-100 h-100 top-50 start-50 translate-middle rounded-top d-flex flex-column align-items-center justify-content-center" style="background: rgba(29, 42, 77, .8);">
@@ -76,7 +148,7 @@
                         <p>Telephone Service</p>
                         <a href="" class="btn btn-primary rounded-pill py-3 px-5 my-2">Apply Now</a>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
