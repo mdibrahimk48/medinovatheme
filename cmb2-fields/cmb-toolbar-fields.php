a3a67871c2f4ade779b6de3210e8adc32b22489d
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-27 19:28:59 +0600

    CMB Fields Functions

--- cmb2-fields/cmb-toolbar-fields.php
+++ cmb2-fields/cmb-toolbar-fields.php
@@ -1,4 +1,31 @@
 <?php
 
+add_action( 'cmb2_init', 'cmb2_toolbar_metabox' );
+function cmb2_toolbar_metabox() {
+
+	$cmb = new_cmb2_box( array(
+		'id'           => $prefix . 'toolbar_metabox',
+		'title'        => __( 'Metabox Title', 'classicmedinova' ),
+		'object_types' => array( 'customtoolbar' ),
+		'context'      => 'normal',
+		'priority'     => 'high',
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Enter Email', 'classicmedinova' ),
+		'id' => $prefix . 'enter_email',
+		'type' => 'text_email',
+		'desc' => __( 'Email Filed', 'classicmedinova' ),
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Social Icon Class', 'classicmedinova' ),
+		'id' => $prefix . 'social_icon_class',
+		'type' => 'text',
+		'desc' => __( 'Enter Social Icon Class', 'classicmedinova' ),
+		'repeatable' => true,
+	) );
+
+}
 
 ?>
