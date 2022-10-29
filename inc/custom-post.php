c1bf587cdcdf0eb5b811dd2c1e4b611a56375d35
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-29 18:15:54 +0600

    Taxonomy Implementation

--- inc/custom-post.php
+++ inc/custom-post.php
@@ -27,4 +27,31 @@ if(!function_exists('mymedicalservice')){
 
 add_action('init', 'mymedicalservice');
 
+
+function wporg_register_taxonomy_course() {
+    $labels = array(
+        'name'              => _x( 'Courses', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Course', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Courses' ),
+        'all_items'         => __( 'All Courses' ),
+        'parent_item'       => __( 'Parent Course' ),
+        'parent_item_colon' => __( 'Parent Course:' ),
+        'edit_item'         => __( 'Edit Course' ),
+        'update_item'       => __( 'Update Course' ),
+        'add_new_item'      => __( 'Add New Course' ),
+        'new_item_name'     => __( 'New Course Name' ),
+        'menu_name'         => __( 'Course' ),
+    );
+    $args   = array(
+        'hierarchical'      => true, // make it hierarchical (like categories)
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'course' ],
+    );
+    register_taxonomy( 'course', [ 'post' ], $args );
+}
+add_action( 'init', 'wporg_register_taxonomy_course' );
+
 ?>
