492c635e7c002b19cbb3dbd038086038f46c5fd8
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-01 20:29:37 +0600

    Social Icon set

--- template-part/content-toolbar.php
+++ template-part/content-toolbar.php
@@ -6,6 +6,7 @@
                         <?php
                             $cell = get_option('_prefix_my_toolbar');
                             $email = get_option('_prefix_my_toolbar');
+                            $social_icon = get_option('_prefix_my_toolbar');
                         ?>
                         <a class="text-decoration-none text-body pe-3" href=""><i class="bi bi-telephone me-2"> <?php echo $cell ['opt-cell']; ?></i></a>
                         <span class="text-body">|</span>
@@ -15,9 +16,9 @@
                 <div class="col-md-6 text-center text-lg-end">
                     <div class="d-inline-flex align-items-center">
                         <a class="text-body px-2" href="">
-                            <i class="fab fa-facebook-f"></i>
+                            <i class="<?php echo $social_icon['opt-icon-class'];?>"></i>
                         </a>
-                        <a class="text-body px-2" href="">
+                        <!-- <a class="text-body px-2" href="">
                             <i class="fab fa-twitter"></i>
                         </a>
                         <a class="text-body px-2" href="">
@@ -28,7 +29,7 @@
                         </a>
                         <a class="text-body ps-2" href="">
                             <i class="fab fa-youtube"></i>
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             </div>
