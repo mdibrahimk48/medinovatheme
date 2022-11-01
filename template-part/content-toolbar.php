574b943e6cc789164f8a3380a77db4df2d39351e
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-02 00:42:22 +0600

    Dynamic Icon

--- template-part/content-toolbar.php
+++ template-part/content-toolbar.php
@@ -6,7 +6,7 @@
                         <?php
                             $cell = get_option('_prefix_my_toolbar');
                             $email = get_option('_prefix_my_toolbar');
-                            $social_icon = get_option('_prefix_my_toolbar');
+                            $social_field = get_option('_prefix_my_toolbar');
                         ?>
                         <a class="text-decoration-none text-body pe-3" href=""><i class="bi bi-telephone me-2"> <?php echo $cell ['opt-cell']; ?></i></a>
                         <span class="text-body">|</span>
@@ -16,7 +16,7 @@
                 <div class="col-md-6 text-center text-lg-end">
                     <div class="d-inline-flex align-items-center">
                         <a class="text-body px-2" href="">
-                            <i class="<?php echo $social_icon['opt-icon-class'];?>"></i>
+                            <i class="<?php echo $social_field['opt-social'];?>"></i>
                         </a>
                         <!-- <a class="text-body px-2" href="">
                             <i class="fab fa-twitter"></i>
