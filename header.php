d373eaef4ae5f828298085ea080dfd01da5802fb
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-03 22:16:56 +0600

    Dynamic Header

--- header.php
+++ header.php
@@ -3,13 +3,17 @@
 
 <head>
     <meta charset="utf-8">
-    <title>MEDINOVA - Hospital Website Template</title>
+    <?php
+        $config = get_option('_prefix_my_options');
+        $string_converter_site_details = $config['opt-site-details'];
+    ?>
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <title><?php echo $string_converter_site_details['opt-customtitle']; ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
-
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
 
     <?php wp_head(); ?>
 
