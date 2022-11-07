3715411ea2d017ae035ba0645316289ea62589bf
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-07 14:55:36 +0600

    CMB2 Group Fields

--- cmb2-fields/cmb-price-fields.php
+++ cmb2-fields/cmb-price-fields.php
@@ -49,6 +49,64 @@ function cmb2_price_metaboxes() {
 		// );
 		// 'display_cb' => 'yourprefix_display_text_small_column', // Output the display of the column values through a callback.
 	) );
+
+
+		/**
+	 * Repeatable Field Groups
+	 */
+	$cmb_group = new_cmb2_box( array(
+		'id'           => 'yourprefix_group_metabox',
+		'title'        => esc_html__( 'Repeating Field Group', 'cmb2' ),
+		'object_types' => array( 'customprice' ),
+	) );
+
+	// $group_field_id is the field id string, so in this case: 'yourprefix_group_demo'
+	$group_field_id = $cmb_group->add_field( array(
+		'id'          => 'yourprefix_group_demo',
+		'type'        => 'group',
+		'description' => esc_html__( 'Generates reusable form entries', 'cmb2' ),
+		'options'     => array(
+			'group_title'    => esc_html__( 'Entry {#}', 'cmb2' ), // {#} gets replaced by row number
+			'add_button'     => esc_html__( 'Add Another Entry', 'cmb2' ),
+			'remove_button'  => esc_html__( 'Remove Entry', 'cmb2' ),
+			'sortable'       => true,
+			// 'closed'      => true, // true to have the groups closed by default
+			// 'remove_confirm' => esc_html__( 'Are you sure you want to remove?', 'cmb2' ), // Performs confirmation before removing group.
+		),
+	) );
+
+	/**
+	 * Group fields works the same, except ids only need
+	 * to be unique to the group. Prefix is not needed.
+	 *
+	 * The parent field's id needs to be passed as the first argument.
+	 */
+	$cmb_group->add_group_field( $group_field_id, array(
+		'name'       => esc_html__( 'Entry Title', 'cmb2' ),
+		'id'         => 'title',
+		'type'       => 'text',
+		// 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+	) );
+
+	$cmb_group->add_group_field( $group_field_id, array(
+		'name'        => esc_html__( 'Description', 'cmb2' ),
+		'description' => esc_html__( 'Write a short description for this entry', 'cmb2' ),
+		'id'          => 'description',
+		'type'        => 'textarea_small',
+	) );
+
+	$cmb_group->add_group_field( $group_field_id, array(
+		'name' => esc_html__( 'Entry Image', 'cmb2' ),
+		'id'   => 'image',
+		'type' => 'file',
+	) );
+
+	$cmb_group->add_group_field( $group_field_id, array(
+		'name' => esc_html__( 'Image Caption', 'cmb2' ),
+		'id'   => 'image_caption',
+		'type' => 'text',
+	) );
 }
+
 
 ?>
