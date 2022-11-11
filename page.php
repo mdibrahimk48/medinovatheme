cc08138ad46e037c632af7dfb4021693776af705
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-11 23:12:34 +0600

    Site Heirarchy

--- page.php
+++ page.php
@@ -6,8 +6,16 @@ get_header();
 
     get_template_part('template-part/content', 'navbar');
 
+    //Single Page Head
     ?>
-            <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5"><?php echo the_title(); ?></h5>
+        <div class="signle_page_title">
+            <div class="single_page_title_left">
+                <h3><?php echo the_title(); ?>:</h3>
+            </div>
+            <div class="single_page_title_right">
+                <h6><a href="<?php echo site_url(); ?>"><i>Home / </i></a><?php echo the_title(); ?></h6>
+            </div>
+        </div>
      <?php
 
 
@@ -15,3 +23,11 @@ get_header();
 get_footer();
 
 ?>
+
+<style>
+    .signle_page_title {
+        display: flex;
+        justify-content: space-between;
+        padding: 10px;
+    }
+</style>
