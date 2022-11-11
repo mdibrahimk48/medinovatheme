ded32916c81b80fb638304e392907ae630144fb0
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-11 22:34:51 +0600

    Dynamic Page Link

--- page.php
+++ page.php
@@ -1,4 +1,17 @@
 <?php
 
+get_header();
+
+    get_template_part('template-part/content', 'toolbar');
+
+    get_template_part('template-part/content', 'navbar');
+
+    ?>
+            <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5"><?php echo the_title(); ?></h5>
+     <?php
+
+
+
+get_footer();
 
 ?>
